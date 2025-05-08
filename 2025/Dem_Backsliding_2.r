5cd8e2af94173ed0325316f0c5e10287d1d92066
Hector <bahamondeh@gmail.com>
2025-05-08 10:49:03 +0300

    wip

--- 2025/Dem_Backsliding_2.r
+++ 2025/Dem_Backsliding_2.r
@@ -1,93 +1,3 @@
-## DO NOT RUN
-cat("\014")
-rm(list=ls())
-setwd("/Users/hectorbahamonde/research/democratic_backsliding/2025/")
-
-# Pacman
-if (!require("pacman")) install.packages("pacman"); library(pacman) 
-
-# p_load(qualtRics)
-
-# this should be run just ONCE per machine
-# qualtrics_api_credentials(api_key = "UPU8fMH4I7Yr5Bz23zuZSPj7RUxRdbEPmB3ubbNo", 
-#                          base_url = "fra1.qualtrics.com",
-#                          install = TRUE,
-#                          overwrite=TRUE)
-
-# surveys <- all_surveys() # to find out each survey id.
-
-# download data
-options(scipen=999999)
-# download data
-# dat <- fetch_survey(surveyID = surveys$id[surveys$name=='Winners and Losers_survey'], verbose = FALSE)
-
-# Translating Data
-
-# install.packages("devtools")
-# devtools::install_github("zumbov2/deeplr")
-# library(deeplr)
-
-# Load required packages
-p_load(deeplr,progress)
-
-# Function to translate text with progress bar
-translate_text <- function(text, pb) {
-  if (!is.na(text) && is.character(text) && nzchar(text)) {  # Ensure it is a character
-    tryCatch({
-      result <- deeplr::translate(text = text,
-                                  source_lang = "FI",
-                                  target_lang = "EN",
-                                  auth_key = "048783fe-31b8-4d6c-bfbc-0a5fd51f9c46")
-      if (!pb$finished) pb$tick()  # Only tick if progress is not finished
-      return(result)
-    }, error = function(e) {
-      warning(paste("Translation error:", e$message))
-      if (!pb$finished) pb$tick()  # Prevent over-ticking
-      return(text) # Return original text if translation fails
-    })
-  } else {
-    if (!pb$finished) pb$tick()  # Update progress bar for NA or non-character values
-    return(text)
-  }
-}
-
-# dat.fi = dat
-
-# Ensure all columns are character type before translation
-# dat.fi <- dat.fi %>%
-#  mutate(across(where(is.factor), as.character)) %>%  # Convert factors to characters
-#  mutate(across(where(is.numeric), as.character))    # Convert numbers to characters
-
-# Correctly count the number of translatable elements (only character and non-NA values)
-# num_elements <- sum(sapply(dat.fi, function(col) sum(!is.na(col) & is.character(col))))
-
-# Initialize progress bar
-# pb <- progress_bar$new(
-#  format = "  Translating [:bar] :percent ETA: :eta",
-#  total = num_elements,
-#  clear = FALSE,
-#  width = 60
-#)
-
-# TRANSLATING CODE
-# Apply translation function with progress bar (only on character columns)
-# dat.fi <- dat.fi %>%  mutate(across(where(is.character), ~ sapply(., function(x) translate_text(x, pb))))
-
-# Ensure progress bar is marked as finished
-# if (!pb$finished) pb$terminate()
-
-# dat.t = dat.fi # duplicating
-# dat.t <- as.data.frame(dat.fi) # formatting
-# dat.t = dat.t[-c(1:12), ] # excluding pre-test
-
-# deletes row names
-# rownames(dat.t) <- NULL
-
-
-# save
-# save(dat.t, file="/Users/hectorbahamonde/research/democratic_backsliding/2025/dat_t.RData")
-
-
 ##########################################################################################
 # Data Cleaning
 ##########################################################################################
@@ -117,6 +27,37 @@ dat.t$Q10_5 = as.factor(dat.t$Q10_5) # no pressure on the judiciary
 dat.t$Q11 = as.factor(dat.t$Q11) # Interest in politics
 dat.t$Q15 = as.factor(dat.t$Q15)
 
+# order Q13_1
+dat.t$Q13_1 <- factor(
+  dat.t$Q13_1,
+  levels = c("I don't know",
+    "Totally disagree",
+    "Somewhat disagree",
+    "Neither agree nor disagree",
+    "Somewhat agree",
+    "Totally agree"
+  ),
+  ordered = TRUE)
+
+# Q13_1.r
+dat.t$Q13_1.r <- ifelse(dat.t$Q13_1 == "Totally disagree", 1, 0)
+
+#
+dat.t$Q13_6 <- factor(
+  dat.t$Q13_6,
+  levels = c(
+    "I don't know",
+    "Totally disagree",
+    "Somewhat disagree",
+    "Neither agree nor disagree",
+    "Somewhat agree",
+    "Totally agree"
+  ),
+  ordered = TRUE
+)
+
+#
+dat.t$Q13_6.r <- ifelse(dat.t$Q13_6 == "Totally disagree", 1, 0)
 
 ### Democracy Support and Priming Variable
 
@@ -201,6 +142,15 @@ ggplot(dat.t, aes(x = Q13_1, fill = Priming)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+ggplot(dat.t, aes(x = Q13_1.r, fill = Priming)) +
+  geom_bar(position = "dodge") + 
+  labs(title = "Distribution of Responses by Framing Condition",
+       x = "The Gov't must be able to censor media that are unduly critical of the Gov't",
+       y = "Count",
+       fill = "Framing Condition") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 ggplot(dat.t, aes(x = Q13_3, fill = Priming)) +
   geom_bar(position = "dodge") + 
   labs(title = "Distribution of Responses by Framing Condition",
@@ -214,6 +164,15 @@ ggplot(dat.t, aes(x = Q13_5, fill = Priming)) +
   geom_bar(position = "dodge") + 
   labs(title = "Distribution of Responses by Framing Condition",
        x = "Government should be able to stretch the limits of legislation to solve pressing social problems",
+       y = "Count",
+       fill = "Framing Condition") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(dat.t, aes(x = Q13_6, fill = Priming)) +
+  geom_bar(position = "dodge") + 
+  labs(title = "Distribution of Responses by Framing Condition",
+       x = "Our country would work better if political decisions were left to independent, non-elected experts instead of politicians",
        y = "Count",
        fill = "Framing Condition") +
   theme_minimal() +
