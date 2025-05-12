2a4fa5280bb9ef02c9ed96c47a208caeccce27d9
Hector <bahamondeh@gmail.com>
2025-05-12 20:49:34 +0300

    wip

--- 2025/Dem_Backsliding_2.r
+++ 2025/Dem_Backsliding_2.r
@@ -17,7 +17,6 @@ p_load(dplyr)
 dat.t <- dat.t %>% mutate(across(where(is.character), ~ ifelse(. == "<NA>", NA, .)))
 
 # cleaning
-dat.t$Q5 = as.factor(dat.t$Q5) # region
 dat.t$Q8_1 = as.numeric(dat.t$Q8_1) # dem satisfaction
 dat.t$Q10_1 = as.factor(dat.t$Q10_1) # dem might have problems, but...
 dat.t$Q10_2 = as.factor(dat.t$Q10_2) # dem is not effective, better a strong leader
@@ -26,6 +25,11 @@ dat.t$Q10_4 = as.factor(dat.t$Q10_4) # pol participation
 dat.t$Q10_5 = as.factor(dat.t$Q10_5) # no pressure on the judiciary
 dat.t$Q11 = as.factor(dat.t$Q11) # Interest in politics
 dat.t$Q15 = as.factor(dat.t$Q15)
+dat.t$Q3 = as.factor(dat.t$Q3) # age
+dat.t$Q4 = as.factor(dat.t$Q4) # gender
+dat.t$Q5 = as.factor(dat.t$Q5) # region
+dat.t$Q6 = as.factor(dat.t$Q6) # educ
+
 
 # order Q13_1
 dat.t$Q13_1 <- factor(
@@ -76,6 +80,93 @@ dat.t$Priming <- recode_factor(dat.t$DisplayOrder,
                                `Q17` = "Winner",
                                #.default = "Control",
                                .missing = "Control")
+####################
+# Polarization
+####################
+
+# Vector of variables corresponding to Q27_1 to Q27_9
+q27_vars <- paste0("Q27_", 1:9)
+
+# Define mapping from character to numeric
+closeness_map <- function(x) {
+  x <- as.character(x)
+  x[x == "Not at all close to 0"] <- "0"
+  x[x == "Very close to 10"] <- "10"
+  x[x == "I don't know"] <- NA
+  as.numeric(x)
+}
+
+# Apply mapping to convert all Q27_* variables to numeric
+for (v in q27_vars) {
+  dat.t[[v]] <- closeness_map(dat.t[[v]])
+}
+
+# Define coalition party positions (based on the variable ordering)
+# Assuming:
+# Q27_1 = SDP
+# Q27_2 = PS (True Finns)
+# Q27_3 = KOK (National Coalition)
+# Q27_4 = KESK
+# Q27_5 = VIHR
+# Q27_6 = VAS
+# Q27_7 = RKP
+# Q27_8 = KD
+# Q27_9 = Movement Now
+
+# coalition_parties as positions (KOK = Q27_3, PS = Q27_2, RKP = Q27_7, KD = Q27_8)
+coalition_indices <- c(2, 3, 7, 8)
+coalition_vars <- q27_vars[coalition_indices]
+
+# Compute average closeness to coalition parties
+dat.t$avg_closeness_coalition <- rowMeans(dat.t[, coalition_vars], na.rm = TRUE)
+
+# Compute ideological distance from government
+dat.t$govt_distance <- 10 - dat.t$avg_closeness_coalition
+
+# Optional: label NA if all coalition ratings were NA
+dat.t$govt_distance[is.nan(dat.t$govt_distance)] <- NA
+
+
+####################
+# Dependent Variable
+####################
+
+# Recode Q13_6 to numeric Likert-style scale
+dat.t$Q13_6_num <- recode(dat.t$Q13_6,
+                          "Totally disagree" = 1,
+                          "Somewhat disagree" = 2,
+                          "Neither agree nor disagree" = 3,
+                          "Somewhat agree" = 4,
+                          "Totally agree" = 5,
+                          "I don't know" = NA_real_)
+
+####################
+# Models
+####################
+
+p_load(jtools)
+# dem satisfaction
+fit = lm(Q13_6_num ~ govt_distance*Q8_1 + Q3 + Q4 + Q5 + Q6, data = dat.t)
+summary(fit)
+effect_plot(fit,
+            pred = "govt_distance",
+            modx = "Q8_1",
+            data = dat.t,
+            interval = TRUE)
+
+dat.t$Q8_1 = as.numeric(dat.t$Q8_1) # dem satisfaction
+dat.t$Q10_1 = as.factor(dat.t$Q10_1) # dem might have problems, but...
+dat.t$Q10_2 = as.factor(dat.t$Q10_2) # dem is not effective, better a strong leader
+dat.t$Q10_3 = as.factor(dat.t$Q10_3) # importance of media
+dat.t$Q10_4 = as.factor(dat.t$Q10_4) # pol participation 
+dat.t$Q10_5 = as.factor(dat.t$Q10_5) # no pressure on the judiciary
+dat.t$Q11 = as.factor(dat.t$Q11) # Interest in politics
+dat.t$Q15 = as.factor(dat.t$Q15)
+dat.t$Q3 = as.factor(dat.t$Q3) # age
+dat.t$Q4 = as.factor(dat.t$Q4) # gender
+dat.t$Q5 = as.factor(dat.t$Q5) # region
+dat.t$Q6 = as.factor(dat.t$Q6) # educ
+
 
 
 
@@ -95,6 +186,8 @@ dat.t$CoalitionPartyVote <- ifelse(dat.t$Q15 %in% coalition_parties, "Coalition"
 
 # Convert to factor for ordered categories
 dat.t$CoalitionPartyVote <- factor(dat.t$CoalitionPartyVote, levels = c("Coalition", "Non-Coalition", "Other"))
+
+##
 
 
 
